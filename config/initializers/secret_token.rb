59b5f5c4ca0f74885d7a7fe2f7805ea0a8f8fee3
Yuli Yang <yuli@figmints.com>
2013-11-19 22:34:30 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funbook::Application.config.secret_key_base = '19330229de48b2e612b528191a8be3a202a0c1d04ca006929919f21ae1fd61f8852cb73d0359193bfe7f4782a9d815e3617fc750c618060671bdd50d954d9fb5'
