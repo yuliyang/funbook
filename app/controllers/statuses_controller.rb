1f48f9caf44e149f0c50e63fa7d22e110c4341a0
Yuli Yang <yuli@figmints.com>
2013-11-21 13:55:50 -0500

    status layout

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,13 +2,11 @@ class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :edit, :update, :destroy]
 
   # GET /statuses
-  # GET /statuses.json
   def index
     @statuses = Status.all
   end
 
   # GET /statuses/1
-  # GET /statuses/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class StatusesController < ApplicationController
   end
 
   # POST /statuses
-  # POST /statuses.json
   def create
     @status = Status.new(status_params)
 
     respond_to do |format|
       if @status.save
         format.html { redirect_to @status, notice: 'Status was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @status }
       else
         format.html { render action: 'new' }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /statuses/1
-  # PATCH/PUT /statuses/1.json
   def update
     respond_to do |format|
       if @status.update(status_params)
         format.html { redirect_to @status, notice: 'Status was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /statuses/1
-  # DELETE /statuses/1.json
   def destroy
     @status.destroy
     respond_to do |format|
       format.html { redirect_to statuses_url }
-      format.json { head :no_content }
     end
   end
 
--- app/views/statuses/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @status, :name, :content, :created_at, :updated_at
